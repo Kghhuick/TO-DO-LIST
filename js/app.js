1c0f5e37323369fe7fb7526b1c72c5d07e02c424
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-05 14:14:07 +0200

    add possibility to remove completed tasks

--- js/app.js
+++ js/app.js
@@ -1,87 +1,77 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var input =  document.querySelector("#taskInput");
+  var input = document.querySelector("#taskInput");
   var btn = document.querySelector("#addTaskButton");
   var ul = document.querySelector("ul");
+  var completedBtn = document.querySelector("#removeFinishedTasksButton");
+  
 
+  var value = 0;
 
+  input.oninput = handleInput;
+  function handleInput(event) {
+    value = event.target.value;
+    console.log(value);
+    return value;
+  }
 
+  btn.onclick = handleButton;
+  function handleButton(handleInput) {
+    if (value.length > 5 && value.length < 100) {
 
-  console.log(ul);
-  console.log(btn);
-  console.log(input);
+      var newLi = document.createElement("li");
+      var h = document.createElement("h1")
+      var complete = document.createElement("button");
+      var delet = document.createElement("button");
 
-var value = 0;
+      ul.appendChild(newLi);
+      newLi.appendChild(h)
+      newLi.appendChild(complete);
+      newLi.appendChild(delet);
+      h.innerText = value;
+      delet.classList.add("delete");
+      complete.classList.add("completed");
+      complete.innerText = "Completed"
+      delet.innerText = "Delete"
+      console.log(value);
+      input.value = " ";
 
-input.oninput = handleInput;
- function handleInput(event){
-  value = event.target.value;
-   console.log(value);
-   return value;
-}
+      return;
+    } else {
+      alert("task has to be longer then 5 and shorter then 100");
+    }
+  }
 
+  document.querySelector(".container").addEventListener("mouseover", function() {
 
-btn.onclick = handleButton;
-function handleButton (handleInput){
- if(value.length > 5 && value.length < 100) {
+    if (document.querySelector(".delete") != null) {
+      [...document.querySelectorAll(".delete")].forEach(function(el) {
+        el.addEventListener("click", function() {
+          el.parentNode.remove();
 
-var newLi = document.createElement("li");
-var h = document.createElement("h1")
-var complete = document.createElement("button");
-var delet = document.createElement("button");
+        });
+      });
+      [...document.querySelectorAll(".completed")].forEach(function(el) {
+        el.addEventListener("click", function() {
+          el.classList.add("blue");
+          el.dataset.value = "1";
+          console.log(el);
+        });
 
-ul.appendChild(newLi);
-newLi.appendChild(h)
-newLi.appendChild(complete);
-newLi.appendChild(delet);
-h.innerText = value;
-delet.classList.add("delete");
-complete.classList.add("completed");
-complete.innerText="Completed"
-delet.innerText="Delete"
-console.log(value);
-input.value= " ";
-
-return;
-
-// return ul,delet,complete;
-}
-else {
-  alert("task has to be longer then 5 and shorter then 100");
-}
- }
-
-
-document.querySelector(".container").addEventListener("mouseover",function(handleButton){
-
-if(document.querySelector(".delete")!= null){
- [...document.querySelectorAll(".delete")].forEach(function(el){
-   el.addEventListener("click",function(){
-     el.parentNode.remove();
-
-   });
- });
- [...document.querySelectorAll(".completed")].forEach(function(el){
-  el.addEventListener("click",function(){
-  el.classList.add("blue");
-
-
-
+      });
+    } else {
+      return;
+    }
   });
 
+  completedBtn.onclick = handleCompleted;
+  function handleCompleted() {
+    var toRemove = document.querySelectorAll(".completed");
+    for (var i = 0; i < toRemove.length; i++) {
+      if (toRemove[i].dataset.value == "1") {
+        toRemove[i].parentNode.remove();
+      }
+    }
+
+  }
+
 });
-}
- else {
-  return;
-}
-
-
-});
-
-
-
-
-
-
-
-
-  });
