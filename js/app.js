4b0238fce5eeac347dfcb8dad0e454b97095557e
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-05 12:02:16 +0200

    add possibility to remove task

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var input =  document.querySelector("#taskInput");
   var btn = document.querySelector("#addTaskButton");
   var ul = document.querySelector("ul");
+
+
+
+
   console.log(ul);
   console.log(btn);
   console.log(input);
 
 var value = 0;
-console.log(value);
+
 input.oninput = handleInput;
  function handleInput(event){
   value = event.target.value;
@@ -18,6 +22,8 @@ input.oninput = handleInput;
 
 btn.onclick = handleButton;
 function handleButton (handleInput){
+ if(value.length > 5 && value.length < 100) {
+
 var newLi = document.createElement("li");
 var h = document.createElement("h1")
 var complete = document.createElement("button");
@@ -28,16 +34,40 @@ newLi.appendChild(h)
 newLi.appendChild(complete);
 newLi.appendChild(delet);
 h.innerText = value;
+delet.classList.add("delete");
 complete.innerText="Completed"
 delet.innerText="Delete"
-
-
-
-
-
 console.log(value);
 input.value= " ";
+
+
+return ul,delet;
 }
+else {
+  alert("task has to be longer then 5 and shorter then 100");
+}
+ }
+
+
+document.querySelector(".container").addEventListener("mouseover",function(handleButton){
+
+if(document.querySelector(".delete")!= null){
+ [...document.querySelectorAll(".delete")].forEach(function(el){
+   el.addEventListener("click",function(){
+     el.parentNode.remove();
+
+   });
+ });
+
+
+
+}
+  else{
+    return;
+ }
+
+
+});
 
 
 
