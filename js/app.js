9f0728a1989aa04078198b74183ff681a0a320ab
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-05 10:12:53 +0200

    add events

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function(event) {
+  var input =  document.querySelector("#taskInput");
+  var btn = document.querySelector("#addTaskButton");
+  var ul = document.querySelector("ul");
+  console.log(ul);
+  console.log(btn);
+  console.log(input);
+
+var value = 0;
+console.log(value);
+input.oninput = handleInput;
+ function handleInput(event){
+  value = event.target.value;
+   console.log(value);
+   return value;
+}
+
+
+btn.onclick = handleButton;
+function handleButton (handleInput){
+var newLi = document.createElement("li");
+var h = document.createElement("h1")
+var complete = document.createElement("button");
+var delet = document.createElement("button");
+
+ul.appendChild(newLi);
+newLi.appendChild(h)
+newLi.appendChild(complete);
+newLi.appendChild(delet);
+h.innerText = value;
+complete.innerText="Completed"
+delet.innerText="Delete"
+
+
+
+
+
+console.log(value);
+input.value= " ";
+}
 
 
 
@@ -9,6 +46,4 @@
 
 
 
-
-
-
+  });
